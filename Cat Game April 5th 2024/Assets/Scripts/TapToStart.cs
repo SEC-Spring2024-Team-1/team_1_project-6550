427a6a7c91669b9dee36cb696f061408508c9dcf
Shreyas Anamanapudi <008456728@coyote.csusb.edu>
2024-05-02 15:10:53 -0700

    AddedSoundonPlayButton

--- Cat Game April 5th 2024/Assets/Scripts/TapToStart.cs
+++ Cat Game April 5th 2024/Assets/Scripts/TapToStart.cs
@@ -4,10 +4,22 @@ using UnityEngine.SceneManagement;
 public class LoadGameButton : MonoBehaviour
 {
     public AudioSource playButton;
+
+    // Adjust this delay time according to the length of your audio clip
+    public float delayBeforeLoad = 0.25f;
+
     public void LoadGameScene()
     {
+        // Play the audio
         playButton.Play();
-        // Load the "game" scene when the button is tapped
+
+        // Delay the scene transition
+        Invoke("LoadSceneWithDelay", delayBeforeLoad);
+    }
+
+    private void LoadSceneWithDelay()
+    {
+        // Load the "game" scene after the specified delay
         SceneManager.LoadScene("game");
     }
 }
